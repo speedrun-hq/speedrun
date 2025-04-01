20ae05b8cc213a298c44b9126d90a9d9b3a1e749
Peter Lee <peter@zetachain.com>
2025-03-31 18:47:29 -0700

    update some env vars

--- api/config/config.go
+++ api/config/config.go
@@ -26,12 +26,12 @@ type Config struct {
 func LoadConfig() (*Config, error) {
 	// Load .env file if it exists
 	_ = godotenv.Load()
-
+	// TODO: Add real support for multiple chains
 	config := &Config{
 		Port:             getEnvOrDefault("PORT", "8080"),
 		DatabaseURL:      getEnvOrDefault("DATABASE_URL", "postgresql://localhost:5432/zetafast?sslmode=disable"),
-		ZetaChainRPCURL:  getEnvOrDefault("ZETACHAIN_RPC_URL", "http://localhost:1317"),
-		ZetaChainChainID: getEnvOrDefault("ZETACHAIN_CHAIN_ID", "testnet-1"),
+		ZetaChainRPCURL:  getEnvOrDefault("ZETACHAIN_RPC_URL", "https://zetachain-athens-evm.blockpi.network/v1/rpc/public"),
+		ZetaChainChainID: getEnvOrDefault("ZETACHAIN_CHAIN_ID", "7001"),
 		SupportedChains:  []string{"base", "polygon"}, // Default supported chains
 	}
 
