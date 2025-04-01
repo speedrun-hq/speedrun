340ce5a076e8242910ecd2520b113c4d2c762040
lumtis <lucas.bertrand.22@gmail.com>
2025-04-01 10:55:12 +0200

    initialize router

--- contracts/evm/src/Router.sol
+++ contracts/evm/src/Router.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 /**
  * @title Router
  * @dev Routes CCTX and handles ZRC20 swaps on ZetaChain
  */
-contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable, AccessControlUpgradeable {
+    // Mapping from chain ID to intent contract address
+    mapping(uint256 => address) public intentContracts;
+
+    // Event emitted when an intent contract is set
+    event IntentContractSet(uint256 indexed chainId, address indexed intentContract);
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -18,9 +25,33 @@ contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     function initialize() public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
+        __AccessControl_init();
+
+        // Grant the default admin role to the deployer
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /**
+     * @dev Sets the intent contract address for a specific chain
+     * @param chainId The chain ID to set the intent contract for
+     * @param intentContract The address of the intent contract
+     */
+    function setIntentContract(uint256 chainId, address intentContract) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(intentContract != address(0), "Invalid intent contract address");
+        intentContracts[chainId] = intentContract;
+        emit IntentContractSet(chainId, intentContract);
+    }
+
+    /**
+     * @dev Gets the intent contract address for a specific chain
+     * @param chainId The chain ID to get the intent contract for
+     * @return The address of the intent contract
+     */
+    function getIntentContract(uint256 chainId) external view returns (address) {
+        return intentContracts[chainId];
+    }
 
     // TODO: Add routing functions
     // - route CCTX
--- contracts/evm/test/Router.t.sol
+++ contracts/evm/test/Router.t.sol
@@ -11,6 +11,9 @@ contract RouterTest is Test {
     address public owner;
     address public user1;
     address public user2;
+    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+
+    event IntentContractSet(uint256 indexed chainId, address indexed intentContract);
 
     function setUp() public {
         owner = address(this);
@@ -35,6 +38,56 @@ contract RouterTest is Test {
 
     function test_Initialization() public {
         assertEq(router.owner(), owner);
+    }
+
+    function test_SetIntentContract() public {
+        uint256 chainId = 1;
+        router.setIntentContract(chainId, user1);
+        assertEq(router.intentContracts(chainId), user1);
+    }
+
+    function test_SetIntentContract_EmitsEvent() public {
+        uint256 chainId = 1;
+        vm.expectEmit(true, true, false, false);
+        emit IntentContractSet(chainId, user1);
+        router.setIntentContract(chainId, user1);
+    }
+
+    function test_SetIntentContract_NonAdminReverts() public {
+        uint256 chainId = 1;
+        vm.prank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                bytes4(keccak256("AccessControlUnauthorizedAccount(address,bytes32)")),
+                user1,
+                DEFAULT_ADMIN_ROLE
+            )
+        );
+        router.setIntentContract(chainId, user2);
+    }
+
+    function test_SetIntentContract_ZeroAddressReverts() public {
+        uint256 chainId = 1;
+        vm.expectRevert("Invalid intent contract address");
+        router.setIntentContract(chainId, address(0));
+    }
+
+    function test_GetIntentContract() public {
+        uint256 chainId = 1;
+        router.setIntentContract(chainId, user1);
+        assertEq(router.getIntentContract(chainId), user1);
+    }
+
+    function test_UpdateIntentContract() public {
+        uint256 chainId = 1;
+        router.setIntentContract(chainId, user1);
+        router.setIntentContract(chainId, user2);
+        assertEq(router.intentContracts(chainId), user2);
+    }
+
+    function test_GetIntentContract_UnsetChainId() public {
+        uint256 chainId = 999;
+        assertEq(router.getIntentContract(chainId), address(0));
     }
 
     // TODO: Add more tests for:
