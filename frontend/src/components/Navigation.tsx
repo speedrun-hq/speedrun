37c2097f46a7d9ac3fae11e8bc26b4be09fd8900
Peter Lee <peter@zetachain.com>
2025-04-01 16:16:29 -0700

    fix nagivation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,26 +3,28 @@ import { ConnectWallet } from './ConnectWallet';
 
 const Navigation = () => {
   return (
-    <nav className="bg-black border-b-4 border-primary-500">
+    <nav className="bg-black border-b-4 border-primary-500 relative z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16 items-center">
-          <Link href="/" className="arcade-text text-xl text-primary-500 hover:text-primary-400">
+          <Link href="/" className="arcade-text text-xl text-primary-500 hover:text-primary-400 relative z-10">
             SPEEDRUN
           </Link>
-          <div className="flex space-x-4 items-center">
+          <div className="flex space-x-4 items-center relative z-10">
             <Link
               href="/intents"
-              className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500"
+              className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500 relative"
             >
               RUNS
             </Link>
             <Link
               href="/create"
-              className="arcade-btn"
+              className="arcade-btn relative"
             >
               NEW RUN
             </Link>
-            <ConnectWallet />
+            <div className="relative">
+              <ConnectWallet />
+            </div>
           </div>
         </div>
       </div>
