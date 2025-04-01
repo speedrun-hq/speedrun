6c6290f37341066c29814df6d8b5480e86b8e259
lumtis <lucas.bertrand.22@gmail.com>
2025-04-01 11:20:53 +0200

    route token management

--- contracts/evm/src/Router.sol
+++ contracts/evm/src/Router.sol
@@ -14,8 +14,29 @@ contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable, AccessCon
     // Mapping from chain ID to intent contract address
     mapping(uint256 => address) public intentContracts;
 
+    // Mapping from token name to whether it exists
+    mapping(string => bool) private _supportedTokens;
+    // Mapping from ZRC20 address to token name
+    mapping(address => string) public zrc20ToTokenName;
+    // Mapping from token name and chain ID to asset address
+    mapping(string => mapping(uint256 => address)) private _tokenAssets;
+    // Mapping from token name and chain ID to ZRC20 address
+    mapping(string => mapping(uint256 => address)) private _tokenZrc20s;
+    // List of supported token names
+    string[] public tokenNames;
+    // List of chain IDs for each token
+    mapping(string => uint256[]) private _tokenChainIds;
+
     // Event emitted when an intent contract is set
     event IntentContractSet(uint256 indexed chainId, address indexed intentContract);
+    // Event emitted when a new token is added
+    event TokenAdded(string indexed name);
+    // Event emitted when a token association is added
+    event TokenAssociationAdded(string indexed name, uint256 indexed chainId, address asset, address zrc20);
+    // Event emitted when a token association is updated
+    event TokenAssociationUpdated(string indexed name, uint256 indexed chainId, address asset, address zrc20);
+    // Event emitted when a token association is removed
+    event TokenAssociationRemoved(string indexed name, uint256 indexed chainId);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -38,7 +59,7 @@ contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable, AccessCon
      * @param chainId The chain ID to set the intent contract for
      * @param intentContract The address of the intent contract
      */
-    function setIntentContract(uint256 chainId, address intentContract) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setIntentContract(uint256 chainId, address intentContract) public onlyRole(DEFAULT_ADMIN_ROLE) {
         require(intentContract != address(0), "Invalid intent contract address");
         intentContracts[chainId] = intentContract;
         emit IntentContractSet(chainId, intentContract);
@@ -49,8 +70,167 @@ contract Router is Initializable, UUPSUpgradeable, OwnableUpgradeable, AccessCon
      * @param chainId The chain ID to get the intent contract for
      * @return The address of the intent contract
      */
-    function getIntentContract(uint256 chainId) external view returns (address) {
+    function getIntentContract(uint256 chainId) public view returns (address) {
         return intentContracts[chainId];
+    }
+
+    /**
+     * @dev Adds a new supported token
+     * @param name The name of the token (e.g., "USDC")
+     */
+    function addToken(string calldata name) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(bytes(name).length > 0, "Token name cannot be empty");
+        require(!_supportedTokens[name], "Token already exists");
+        
+        _supportedTokens[name] = true;
+        tokenNames.push(name);
+        emit TokenAdded(name);
+    }
+
+    /**
+     * @dev Adds a new token association
+     * @param name The name of the token
+     * @param chainId The chain ID where the asset exists
+     * @param asset The ERC20 address on the source chain
+     * @param zrc20 The ZRC20 address on ZetaChain
+     */
+    function addTokenAssociation(
+        string calldata name,
+        uint256 chainId,
+        address asset,
+        address zrc20
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_supportedTokens[name], "Token does not exist");
+        require(asset != address(0), "Invalid asset address");
+        require(zrc20 != address(0), "Invalid ZRC20 address");
+        require(_tokenAssets[name][chainId] == address(0), "Association already exists");
+        
+        _tokenAssets[name][chainId] = asset;
+        _tokenZrc20s[name][chainId] = zrc20;
+        _tokenChainIds[name].push(chainId);
+        zrc20ToTokenName[zrc20] = name;
+        
+        emit TokenAssociationAdded(name, chainId, asset, zrc20);
+    }
+
+    /**
+     * @dev Updates an existing token association
+     * @param name The name of the token
+     * @param chainId The chain ID where the asset exists
+     * @param asset The new ERC20 address on the source chain
+     * @param zrc20 The new ZRC20 address on ZetaChain
+     */
+    function updateTokenAssociation(
+        string calldata name,
+        uint256 chainId,
+        address asset,
+        address zrc20
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_supportedTokens[name], "Token does not exist");
+        require(asset != address(0), "Invalid asset address");
+        require(zrc20 != address(0), "Invalid ZRC20 address");
+        require(_tokenAssets[name][chainId] != address(0), "Association does not exist");
+        
+        _tokenAssets[name][chainId] = asset;
+        _tokenZrc20s[name][chainId] = zrc20;
+        zrc20ToTokenName[zrc20] = name;
+        
+        emit TokenAssociationUpdated(name, chainId, asset, zrc20);
+    }
+
+    /**
+     * @dev Removes a token association
+     * @param name The name of the token
+     * @param chainId The chain ID to remove the association for
+     */
+    function removeTokenAssociation(
+        string calldata name,
+        uint256 chainId
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_supportedTokens[name], "Token does not exist");
+        require(_tokenAssets[name][chainId] != address(0), "Association does not exist");
+        
+        delete _tokenAssets[name][chainId];
+        delete _tokenZrc20s[name][chainId];
+        
+        // Remove chainId from the array
+        uint256[] storage chainIds = _tokenChainIds[name];
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            if (chainIds[i] == chainId) {
+                chainIds[i] = chainIds[chainIds.length - 1];
+                chainIds.pop();
+                break;
+            }
+        }
+        
+        emit TokenAssociationRemoved(name, chainId);
+    }
+
+    /**
+     * @dev Gets the token association for a specific chain
+     * @param zrc20 The ZRC20 address on ZetaChain
+     * @param chainId The chain ID to get the association for
+     * @return asset The ERC20 address on the source chain
+     * @return zrc20Addr The ZRC20 address on ZetaChain
+     * @return chainIdValue The chain ID where the asset exists
+     */
+    function getTokenAssociation(
+        address zrc20,
+        uint256 chainId
+    ) public view returns (
+        address asset,
+        address zrc20Addr,
+        uint256 chainIdValue
+    ) {
+        string memory name = zrc20ToTokenName[zrc20];
+        require(_supportedTokens[name], "Token does not exist");
+        require(_tokenAssets[name][chainId] != address(0), "Association does not exist");
+        
+        return (_tokenAssets[name][chainId], _tokenZrc20s[name][chainId], chainId);
+    }
+
+    /**
+     * @dev Gets all token associations for a specific token
+     * @param name The name of the token
+     * @return chainIds Array of chain IDs
+     * @return assets Array of asset addresses
+     * @return zrc20s Array of ZRC20 addresses
+     */
+    function getTokenAssociations(string calldata name) public view returns (
+        uint256[] memory chainIds,
+        address[] memory assets,
+        address[] memory zrc20s
+    ) {
+        require(_supportedTokens[name], "Token does not exist");
+        
+        chainIds = _tokenChainIds[name];
+        uint256 length = chainIds.length;
+        
+        assets = new address[](length);
+        zrc20s = new address[](length);
+        
+        for (uint256 i = 0; i < length; i++) {
+            uint256 chainId = chainIds[i];
+            assets[i] = _tokenAssets[name][chainId];
+            zrc20s[i] = _tokenZrc20s[name][chainId];
+        }
+    }
+
+    /**
+     * @dev Gets all supported token names
+     * @return Array of token names
+     */
+    function getSupportedTokens() public view returns (string[] memory) {
+        return tokenNames;
+    }
+
+    /**
+     * @dev Checks if a token exists
+     * @param name The name of the token
+     * @return Whether the token exists
+     */
+    function isTokenSupported(string calldata name) public view returns (bool) {
+        return _supportedTokens[name];
     }
 
     // TODO: Add routing functions
