6c3ea0dd5aa82c797c86f39a7c28f16a9eb050ef
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2025-04-04 10:51:21 +0200

    feat: add a leaderboard page (#50)
    * fix wallet connect ui
    
    * add leaderboard page

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -6,21 +6,27 @@ const Navigation = () => {
     <nav className="bg-black border-b-4 border-primary-500 relative z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16 items-center">
-          <Link href="/" className="arcade-text text-xl text-primary-500 hover:text-primary-400 relative z-10">
-            SPEEDRUN
+          <Link href="/" className="arcade-text text-2xl text-primary-500 hover:text-primary-400 relative z-10 font-bold">
+          SPEEDRUN
           </Link>
           <div className="flex space-x-4 items-center relative z-10">
+            <Link
+              href="/"
+              className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500 relative"
+            >
+              MAKE TRANSFER
+            </Link>
+            <Link
+              href="/leaderboard"
+              className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500 relative"
+            >
+              LEADERBOARD
+            </Link>
             <Link
               href="/about"
               className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500 relative"
             >
               LEARN MORE
-            </Link>
-            <Link
-              href="/intents"
-              className="arcade-btn border-yellow-500 text-yellow-500 hover:bg-yellow-500 relative"
-            >
-              TRANSFERS
             </Link>
             <div className="relative">
               <ConnectWallet />
