8b20e08a013a34ed3a1d0767f970195ce431a7a0
Peter Lee <peter@zetachain.com>
2025-04-11 09:40:34 -0700

    format

--- frontend/src/components/WarningBanner.tsx
+++ frontend/src/components/WarningBanner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const WarningBanner: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -10,34 +10,36 @@ const WarningBanner: React.FC = () => {
   return (
     <div className="bg-yellow-600 text-white py-2 px-4 text-center relative z-50">
       <div className="container mx-auto flex items-center justify-center text-xs md:text-sm">
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          className="h-5 w-5 mr-2 flex-shrink-0" 
-          viewBox="0 0 20 20" 
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mr-2 flex-shrink-0"
+          viewBox="0 0 20 20"
           fill="currentColor"
         >
-          <path 
-            fillRule="evenodd" 
-            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" 
-            clipRule="evenodd" 
+          <path
+            fillRule="evenodd"
+            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+            clipRule="evenodd"
           />
         </svg>
-        <span>WARNING: Smart contracts have not been audited. Use at your own risk.</span>
-        <button 
-          onClick={() => setIsVisible(false)} 
+        <span>
+          WARNING: Smart contracts have not been audited. Use at your own risk.
+        </span>
+        <button
+          onClick={() => setIsVisible(false)}
           className="ml-4 text-white hover:text-gray-200 focus:outline-none"
           aria-label="Close warning"
         >
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            className="h-4 w-4" 
-            viewBox="0 0 20 20" 
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            viewBox="0 0 20 20"
             fill="currentColor"
           >
-            <path 
-              fillRule="evenodd" 
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" 
-              clipRule="evenodd" 
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
             />
           </svg>
         </button>
@@ -46,4 +48,4 @@ const WarningBanner: React.FC = () => {
   );
 };
 
-export default WarningBanner; 
+export default WarningBanner;
