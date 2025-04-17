9d404c774827db500e414fd8ba766d4aaf69e06e
lumtis <lucas.bertrand.22@gmail.com>
2025-04-17 18:45:18 +0200

    refactor: update banner

--- frontend/src/components/WarningBanner.tsx
+++ frontend/src/components/WarningBanner.tsx
@@ -8,23 +8,27 @@ const WarningBanner: React.FC = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="bg-yellow-600 text-white py-2 px-4 text-center relative z-50">
-      <div className="container mx-auto flex items-center justify-center text-xs md:text-sm">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 mr-2 flex-shrink-0"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <span>
-          WARNING: Smart contracts have not been audited. Use at your own risk.
-        </span>
+    <div className="flex justify-center relative z-50">
+      <div className="bg-yellow-600 text-white py-2 px-6 rounded-b-lg text-center text-xs md:text-sm inline-flex items-center shadow-md">
+        <div className="flex flex-col">
+          <div className="flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-1 flex-shrink-0"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="font-bold">WARNING: You're using a live alpha.</span>
+          </div>
+          <span>Smart contracts have not been audited. Use at your own risk.</span>
+          <span>Some features may be unstable as we ship updates quickly.</span>
+        </div>
         <button
           onClick={() => setIsVisible(false)}
           className="ml-4 text-white hover:text-gray-200 focus:outline-none"
