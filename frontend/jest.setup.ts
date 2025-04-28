ea12895fa56eb845f6d262647a9b6998e0fdbbe9
Peter Lee <peter@zetachain.com>
2025-04-28 12:39:18 -0700

    fix lint error

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -15,8 +15,9 @@ jest.mock("next/navigation", () => ({
   useSearchParams: () => new URLSearchParams(),
 }));
 
-// Extend Jest matchers
+// Extend Jest matchers with modern module syntax
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
       toBeInTheDocument(): R;
