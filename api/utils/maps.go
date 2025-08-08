d37918fdd995d7e76b8a051b2abfff11e3e6f2b8
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-08-08 11:41:00 +0300

    Add docs for MapMap

--- api/utils/maps.go
+++ api/utils/maps.go
@@ -1,7 +1,11 @@
 package utils
 
-func MapMap[K comparable, V any, R any](m map[K]V, f func(K, V) R) map[K]R {
-	res := make(map[K]R, len(m))
+// MapMap maps a map to a new map using a function. Examples:
+// - map[string]int => map[string][]byte
+// - map[int]bool => map[int]string
+// - map[string]*MyStruct => map[string]io.Reader
+func MapMap[KEY comparable, A any, B any](m map[KEY]A, f func(KEY, A) B) map[KEY]B {
+	res := make(map[KEY]B, len(m))
 
 	for k, v := range m {
 		res[k] = f(k, v)
